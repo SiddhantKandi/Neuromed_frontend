082d3786ae88597924eb3a86f87c0f434c68dcc2
Siddhant kandi <siddhantkandi.intern@peopleprudent.co.in>
2025-03-17 22:00:42 +0530

    add AppointmentMenu component to Home page for booking appointments

---
+++ src/components/AppointmentMenu.tsx
@@ -0,0 +1,28 @@
+import { assets } from "../assets/assets_frontend/assets"
+import { useNavigate } from 'react-router-dom';
+
+function AppointmentMenu() {
+
+    const navigate = useNavigate();
+
+  return (
+    <div className="flex items-center justify-between bg-indigo-700 rounded-lg mx-20 p-6 h-60">
+      {/* Left Section */}
+      <div className="flex flex-col justify-center space-y-4 text-white w-2/3">
+        <h2 className="text-3xl font-bold">Book Appointment With 100+ Trusted Doctors</h2>
+        <button className="px-4 text-sm py-3 bg-white text-indigo-700 font-semibold rounded-full shadow-md w-40 hover:cursor-pointer transition-transform hover:scale-105 duration-300 ml-3"
+        onClick={() => navigate('/login')}>
+          Create Account
+        </button>
+      </div>
+
+      {/* Right Section */}
+      <div className="w-1/3 flex justify-center">
+        <img className="h-64 object-contain" src={assets.appointment_img} alt="Appointment Image"/>
+      </div>
+    </div>
+  )
+}
+
+export default AppointmentMenu;
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import DoctorMenu from '../components/DoctorMenu'
+import AppointmentMenu from '../components/AppointmentMenu'
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
         <Header />
         <SpecialityMenu />
         <DoctorMenu />
+        <AppointmentMenu />
       </div>
   )
 }
